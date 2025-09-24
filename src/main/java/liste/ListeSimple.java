a2ebac822d987d59491add06c0539a9053398f0b
Elyes Chouchane <100144302+elyesChouchane@users.noreply.github.com>
2025-09-24 16:35:08 +0200

    Update ListeSimple.java

--- src/main/java/liste/ListeSimple.java
+++ src/main/java/liste/ListeSimple.java
@@ -121,7 +121,8 @@ class ListeSimple {
     void echanger(Noeud r1, Noeud r2) {
         if (r1 == r2)
             return;
-        Noeud precedentR1, precedentR2;
+        Noeud precedentR1;
+        Noeud precedentR2;
         if (r1 != tete && r2 != tete) {
             precedentR1 = getPrecedent(r1);
             precedentR2 = getPrecedent(r2);
